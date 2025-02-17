5fda6b13b9cf245ef5d3d28c134e3871a2a560c9
Omid Jafari <omid.jafari@bcm.edu>
2025-02-17 10:17:27 -0600

    Fix typo

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       <div className="text-center pb-3">
         <h2 className="text-3xl  font-bold tracking-tight text-gray-900 sm:text-4xl">About</h2>
       </div>
-      <p className="text-left text-gray-800">We are interested in conducting clinical epidemiology and health services research using "big data", ranging from administrative claims database from public and private payers to electronic health records (EHR) from multiple U.S. hospital systems.</p>
+      <p className="text-left text-gray-800">We are interested in conducting clinical epidemiology and health services research using big data, ranging from administrative claims database from public and private payers to electronic health records (EHR) from multiple U.S. hospital systems.</p>
     </>
   );
 }
