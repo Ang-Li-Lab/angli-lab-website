cb1207513d82c08043b0d3960f1925b4d0e40b73
Omid Jafari <omid.jafari@bcm.edu>
2025-05-15 18:12:04 -0500

    Added link to GitHub account.

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -7,7 +7,21 @@ export default function Projects() {
       <div className="text-center pb-3">
         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Our Projects</h2>
       </div>
-      <p className="text-left pb-3 text-gray-800">Click on a card to view the corresponding project.</p>
+
+      <div className="pb-3 text-left text-gray-800 space-y-2">
+        <p>Click on a card to view the corresponding project.</p>
+        <p>
+          Some of our project source codes are publicly available on our{" "}
+          <a
+            href="https://github.com/Ang-Li-Lab"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-600 hover:underline"
+          >
+            GitHub account
+          </a>.
+        </p>
+      </div>
 
       <ProjectsComp projectsData={ projectsData } />
     </>
