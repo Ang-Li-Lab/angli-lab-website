2060f9e5a7d202079fb45458be70f1136ab2aca3
Omid Jafari <omid.jafari@bcm.edu>
2025-05-15 18:16:38 -0500

    Layout bugfix

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function HeaderComp() {
   return (
     <header className="bg-customBlue text-white p-4">
-      <nav className="container mx-auto flex justify-between">
+      <nav className="container mx-auto flex justify-between pl-[110px]">
         <a className="logo" href="https://bcm.edu"></a>
         <div className="main-title text-xl">Ang Li Lab</div>
         <div className="space-x-4">
