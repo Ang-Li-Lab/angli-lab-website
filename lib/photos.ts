11ae02030bb365a7fabd00bc2f6bfcd5b65d2852
Omid Jafari <omid.jafari@bcm.edu>
2025-12-10 13:13:32 -0600

    Added more photos
    Signed-off-by: Omid Jafari <omid.jafari@bcm.edu>

--- lib/photos.ts
+++ lib/photos.ts
@@ -5,35 +5,44 @@ import rice_ai_in_health_conf_2024 from "@/public/rice_ai_in_health_conf_2024.jp
 import ash_2024 from "@/public/ash_2024.jpg";
 import htrs_2025 from "@/public/htrs_2025.jpeg";
 import aimahead_2025 from "@/public/aimahead_2025.jpg";
-
+import ash_2025_barbara_poster from "@/public/ash_2025_barbara_poster.jpeg";
+import ash_2025_joyce_oral from "@/public/ash_2025_joyce_oral.jpeg";
 
 export const photosData = [
   {
-    description: 'Ang Li Lab members at ASH 2023 (L to R: Shengling Ma (postdoc), Jun "Mark" Jiang (Fellow), Ang Li, Rockbum Kim (postdoc), Hanqing "Kathy" (PGY3)',
-    imageUrl: ash_2023,
+    description: "Joyce Tiong presenting at ASH 2025 Conference",
+    imageUrl: ash_2025_joyce_oral,
   },
   {
-    description: "Dr. Li Receiving ASCO Career Development Award",
-    imageUrl: asco_award_2024,
+    description: "Barbara Lam presenting at ASH 2025 Conference",
+    imageUrl: ash_2025_barbara_poster,
   },
   {
-    description: "Dr. Li Presenting at ICTHIC 2024",
-    imageUrl: icthic_2024,
-  },
-  {
-    description: '2024 AI in Health Conference (L to R: Omid Jafari, Jun "Mark" Jiang)',
-    imageUrl: rice_ai_in_health_conf_2024,
-  },
-  {
-    description: "Mark presenting at ASH 2024 Conference",
-    imageUrl: ash_2024,
+    description: "2025 AIM-AHEAD Annual Meeting (L to R: Omid Jafari, Abiodun Oluyomi, Ang Li)",
+    imageUrl: aimahead_2025,
   },
   {
     description: "Mrinal presenting at HTRS 2025",
     imageUrl: htrs_2025,
   },
   {
-    description: "2025 AIM-AHEAD Annual Meeting (L to R: Omid Jafari, Abiodun Oluyomi, Ang Li)",
-    imageUrl: aimahead_2025,
+    description: "Mark presenting at ASH 2024 Conference",
+    imageUrl: ash_2024,
+  },
+  {
+    description: '2024 AI in Health Conference (L to R: Omid Jafari, Jun "Mark" Jiang)',
+    imageUrl: rice_ai_in_health_conf_2024,
+  },
+  {
+    description: "Dr. Li Presenting at ICTHIC 2024",
+    imageUrl: icthic_2024,
+  },
+  {
+    description: "Dr. Li Receiving ASCO Career Development Award",
+    imageUrl: asco_award_2024,
+  },
+  {
+    description: 'Ang Li Lab members at ASH 2023 (L to R: Shengling Ma (postdoc), Jun "Mark" Jiang (Fellow), Ang Li, Rockbum Kim (postdoc), Hanqing "Kathy" (PGY3)',
+    imageUrl: ash_2023,
   },
 ];
